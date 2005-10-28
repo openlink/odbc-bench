e82faa1ea7220c454961b270c32be7ec21850a5d
Patrick van Kleef <pkleef@openlinksw.com>
2005-10-28 15:49:20 +0000

    * Added GPL copyright comments in all files (pvk)

--- src/macosx/Dialog.h
+++ src/macosx/Dialog.h
@@ -1,10 +1,24 @@
 /*
  *  Dialog.h
- *  odbc-bench
  *
- *  Created by Farmer Joe on 10/11/05.
- *  Copyright 2005 __MyCompanyName__. All rights reserved.
+ *  $Id$
  *
+ *  odbc-bench - a TPC-A and TPC-C like benchmark program for databases
+ *  Copyright (C) 2000-2005 OpenLink Software <odbc-bench@openlinksw.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef _MACOSX_DIALOG_H_
--- src/macosx/main.cpp
+++ src/macosx/main.cpp
@@ -1,10 +1,25 @@
-//
-//  main.c
-//  odbc-bench
-//
-//  Created by Farmer Joe on 9/25/05.
-//  Copyright __MyCompanyName__ 2005. All rights reserved.
-//
+/*
+ *  main.cpp
+ *
+ *  $Id$
+ *
+ *  odbc-bench - a TPC-A and TPC-C like benchmark program for databases
+ *  Copyright (C) 2000-2005 OpenLink Software <odbc-bench@openlinksw.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <memory>
 
