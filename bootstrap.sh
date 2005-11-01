c1c4bd38bba5a402f0e588083b085393829a0934
Patrick van Kleef <pkleef@openlinksw.com>
2005-11-01 13:07:12 +0000

    * Added check for both libtoolize and glibtoolize for Mac OS X * Added -I flag for aclocal to check macros in admin directory * Added extra blank lines to output   (pvk)

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@
 #  Generate Makefile.in and configure script for distribution
 #
 #  odbc-bench - a TPC-A and TPC-C like benchmark program for databases 
-#  Copyright (C) 2000-2003 OpenLink Software <odbc-bench@openlinksw.com>
+#  Copyright (C) 2000-2005 OpenLink Software <odbc-bench@openlinksw.com>
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -34,30 +34,44 @@ echo "Checking build environment ..."
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`autoconf' installed on your system."
+    echo
     DIE=1
 }
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`automake' installed on your system."
+    echo
     DIE=1
 }
 
-(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+LIBTOOLIZE=
+if test "x$LIBTOOLIZE" = "x"
+then
+	(libtoolize --version) > /dev/null 2>&1 && LIBTOOLIZE=libtoolize
+fi
+if test "x$LIBTOOLIZE" = "x"
+then
+	(glibtoolize --version) > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize
+fi
+if test "x$LIBTOOLIZE" = "x"
+then
     echo
     echo "**Error**: You must have \`libtool' installed on your system."
+    echo
     DIE=1
-}
+fi
 
 (gtk-config --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`GTK' installed on your system."
-    DIE=1
+    echo "*Warning*: You may need \`GTK' installed on your system."
+    echo
 }
 
 (xml2-config --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`xml2' installed on your system."
+    echo
     DIE=1
 }
 
@@ -75,8 +89,8 @@ fi
 #
 #  Generate the build scripts
 #
-echo "Running libtoolize ..."
-libtoolize --force --copy
+echo "Running $LIBTOOLIZE ..."
+$LIBTOOLIZE --force --copy
 if test $? -ne 0
 then
     echo
@@ -85,7 +99,7 @@ then
 fi
 
 echo "Running aclocal ..."
-aclocal
+aclocal -I admin
 if test $? -ne 0
 then
     echo
