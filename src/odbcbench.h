11534fa8a55de237ce802c7e12aa46d523d43407
Patrick van Kleef <pkleef@openlinksw.com>
2005-11-03 08:42:15 +0000

    * Updated version number to 0.99.25 (pvk)

--- src/odbcbench.h
+++ src/odbcbench.h
@@ -61,10 +61,10 @@ extern "C" {
 #define PACKAGE			"odbc-bench"
 #define PACKAGE_BUGREPORT	"odbc-bench@openlinksw.com"
 #define PACKAGE_NAME 		"OpenLink ODBC Benchmark Utility"
-#define PACKAGE_STRING 		"OpenLink ODBC Benchmark Utility 0.99.22"
+#define PACKAGE_STRING 		"OpenLink ODBC Benchmark Utility 0.99.25"
 #define PACKAGE_TARNAME 	"odbc-bench"
-#define PACKAGE_VERSION 	"0.99.22"
-#define VERSION			"0.99.22"
+#define PACKAGE_VERSION 	"0.99.25"
+#define VERSION			"0.99.25"
 #endif
 
 extern void (*pane_log) (const char *format, ...);
